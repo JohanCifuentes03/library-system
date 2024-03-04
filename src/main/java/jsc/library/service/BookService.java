846baa6e9e7cefb4e3778bbf6c858e218e8e921a
johan <johancfuentes03@gmail.com>
2024-03-03 21:01:56 -0500

    deleted function added in book service

--- src/main/java/jsc/library/service/BookService.java
+++ src/main/java/jsc/library/service/BookService.java
@@ -5,6 +5,7 @@ import jsc.library.entity.Book;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class BookService {
     ArrayList<Book> books;
@@ -28,6 +29,17 @@ public class BookService {
             }
         }
         return Optional.empty();
+    }
+
+    public void deleteBookById(Integer id){
+        books = new ArrayList<>(books.stream()
+                .filter(book -> book.getId() != id)
+                .collect(Collectors.toList()));
+        System.out.println("Book successfully deleted");
+    }
+
+    public void updateBook(Book newBook){
+
     }
 
 }
